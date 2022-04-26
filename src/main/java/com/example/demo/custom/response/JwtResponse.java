ba467d6676caf611041b4dd9fcfca8c1d4814d04
duydo8 <duydogch123456@gmail.com>
2022-04-26 08:20:57 +0700

    revert security error

--- src/main/java/com/example/demo/custom/response/JwtResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.custom.response;
-
-import java.util.List;
-
-public class JwtResponse {
-
-    public JwtResponse(String jwt, Long id, String username, String email, List<String> roles) {
-    }
-}
--- src/main/java/com/example/demo/custom/response/MessageReponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.custom.response;
-
-public class MessageReponse {
-}
